a2d589e8e3d739ccfb2545fc6e78dd90765c5f06
lamula21 <jvaldiv8@montgomerycollege.edu>
2024-06-10 15:21:52 -0400

    prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,14 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css"
+import { Inter } from "next/font/google"
 
-import { Chat } from '@/components/chat'
-import { Providers } from '@/components/providers'
+import { Chat } from "@/components/chat"
+import { Providers } from "@/components/providers"
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
-	title: 'Bookbuddy',
-	description: 'Your bookstroe for fantasy & mystery novels',
+	title: "Bookbuddy",
+	description: "Your bookstroe for fantasy & mystery novels",
 }
 
 export default function RootLayout({
